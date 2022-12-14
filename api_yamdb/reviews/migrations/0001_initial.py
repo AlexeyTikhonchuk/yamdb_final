d7d8d326ed869d7a2056f1272eca8734d421b1a9
Alexey Tikhonchuk <alexeytikhonchuk@gmail.com>
2022-12-14 13:42:29 +0300

    first_3

--- api_yamdb/reviews/migrations/0001_initial.py
+++ api_yamdb/reviews/migrations/0001_initial.py
@@ -3,11 +3,10 @@
 import django.core.validators
 import django.db.models.deletion
 import django.utils.timezone
-from django.conf import settings
-from django.db import migrations, models
-
 import reviews.managers
 import reviews.validators
+from django.conf import settings
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
